091f4361a1ff256a2c3ab94c856f2403c9101abe
Sushrut2001 <sushrutsilekar2001@gmail.com>
2025-02-27 15:38:53 +0530

    make dynamic searchbar

--- hooksP/src/App.jsx
+++ hooksP/src/App.jsx
@@ -8,26 +8,40 @@ import Cards from './Components/Cards'
 
 function App() {
   const [data, setData] = useState([])
+  const [search, setSearch] = useState('')
+  const [filteredData, setfilteredData] = useState([])
 
   const displayData = async () => {
     const api = "https://dummyjson.com/products"
     const response = await fetch(api);
     const result = await response.json();
-    setData(result)
+    setData(result.products)
   }
 
   useEffect(() => {
     displayData();
   }, [])
-  console.log(data.products);
+  // console.log(data);
+
+  useEffect(() => {
+    const filterItem = data.filter((product) => {
+      return product.title.toLowerCase().includes(search.toLowerCase());
+    })
+    setfilteredData(filterItem)
+  }, [data,search])
+  console.log(filteredData);
+
+  // const handleChange = (e) => {
+  //   setSearch(e.target.value)
+  // }
 
   return (
     <div>
       <div className='flex justify-center items-center'>
-        <input type="text" className='m-5 p-3 w-[500px] border-none' placeholder='Search what you want...' />
+        <input value={search} onChange={(e)=>setSearch(e.target.value)} type="text" className='m-5 p-3 w-[500px] border-none' placeholder='Search what you want...' />
       </div>
       <div className='grid grid-cols-3 h-full gap-5 m-5 text-center'>
-        {data.products?.map((product) => (
+        {filteredData.map((product) => (
           <Cards products={product} />
         ))}
       </div>
